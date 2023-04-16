ec617c1e8e8413e52b0f4e9e78fbf6246685b633
Mary Marcum <maryrmarcum17@gmail.com>
2023-04-16 15:50:10 -0400

    updated Heading with styling

---
+++ src/Heading.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import { GrCart } from "react-icons/gr";
+
+
+function Heading() {
+    return (
+      <div className="heading-container">
+          <h1>Fake Store</h1>
+          <GrCart id="gr-cart"/>
+        
+          
+  
+      </div>
+    )
+  }
+  
+  export default Heading 
